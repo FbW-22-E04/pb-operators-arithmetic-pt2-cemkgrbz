14366fcc333d4f6d7aa63c42e56b5f41ce9edee6
Cem <cemkgrbz@gmail.com>
2022-06-30 15:30:51 +0200

    added solution.js

---
+++ solution.js
@@ -0,0 +1,63 @@
+//0
+
+const snackServing = 50;
+const guests = 30;
+
+//1
+
+console.log(snackServing + guests);
+
+//2
+
+console.log(guests - snackServing);
+console.log(snackServing - guests);
+
+//3
+
+console.log(snackServing * guests);
+
+//4
+
+console.log(snackServing / guests);
+
+//5
+
+const drinks = 10;
+console.log((snackServing * guests) / drinks);
+
+const resultOne = 150;
+console.log(resultOne);
+
+//6
+
+const monkeys = 15;
+const bananas = 9;
+console.log(monkeys / bananas);
+
+//7
+
+const monkeyFights = 20;
+console.log((monkeys+bananas)*monkeyFights);
+const resultTwo = 480;
+
+//8
+
+let monkeyBusiness = 90;
+console.log(monkeyBusiness++);
+console.log(monkeyBusiness);
+
+//9
+
+let boredom = 13;
+console.log(boredom--);
+console.log(boredom);
+
+//10
+
+console.log(bananas - monkeys);
+const happiness = -6;
+console.log(happiness+monkeyFights)
+
+//11
+
+console.log(resultTwo % resultOne);
